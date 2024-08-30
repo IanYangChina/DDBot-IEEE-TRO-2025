a3b658e3f688350e349cbcfe25ed10819c72f638
xintong <xintongyang1994@foxmail.com>
2024-08-30 12:04:06 +0100

    RL ready

--- scripts/run_rl.sh
+++ scripts/run_rl.sh
@@ -1,5 +1,5 @@
-python run_rl_abs2.py --ptcl-d 1e7 --cuda_GB 6 --t-cuda-id 0 --task-id 0 --seed 0
-python run_rl_abs2.py --ptcl-d 1e7 --cuda_GB 6 --t-cuda-id 0 --task-id 0 --seed 1
-python run_rl_abs2.py --ptcl-d 1e7 --cuda_GB 6 --t-cuda-id 0 --task-id 0 --seed 2
-python run_rl_abs2.py --ptcl-d 1e7 --cuda_GB 6 --t-cuda-id 0 --task-id 0 --seed 3
-python run_rl_abs2.py --ptcl-d 1e7 --cuda_GB 6 --t-cuda-id 0 --task-id 0 --seed 4
+python run_rl_abs2.py --ptcl-d 5e6 --cuda_GB 6 --t-cuda-id 0 --task-id 0 --seed 0
+python run_rl_abs2.py --ptcl-d 5e6 --cuda_GB 6 --t-cuda-id 0 --task-id 0 --seed 1
+python run_rl_abs2.py --ptcl-d 5e6 --cuda_GB 6 --t-cuda-id 0 --task-id 0 --seed 2
+python run_rl_abs2.py --ptcl-d 5e6 --cuda_GB 6 --t-cuda-id 0 --task-id 0 --seed 3
+python run_rl_abs2.py --ptcl-d 5e6 --cuda_GB 6 --t-cuda-id 0 --task-id 0 --seed 4
