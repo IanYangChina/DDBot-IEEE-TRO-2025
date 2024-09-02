1333d8a4a07dedf3700722b20693d6391e0341f5
xintong <xintongyang1994@foxmail.com>
2024-09-02 13:36:31 +0100

    script

--- scripts/run_so_grad.sh
+++ scripts/run_so_grad.sh
@@ -1,1 +1,6 @@
 python run_so_abs2.py --com_grad --ptcl_d 5e7 --cuda_GB 8 --task-id 0
+python run_so_abs2.py --com_grad --ptcl_d 1e7 --cuda_GB 8 --task-id 0
+python run_so_abs2.py --com_grad --ptcl_d 2e7 --cuda_GB 8 --task-id 0
+python run_so_abs2.py --com_grad --ptcl_d 5e7 --cuda_GB 8 --task-id 1
+python run_so_abs2.py --com_grad --ptcl_d 1e7 --cuda_GB 8 --task-id 1
+python run_so_abs2.py --com_grad --ptcl_d 2e7 --cuda_GB 8 --task-id 1
