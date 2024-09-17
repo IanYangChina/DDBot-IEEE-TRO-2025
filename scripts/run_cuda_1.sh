ca9609ecf359ecc4e10da0376442673d0ee4b267
Xintong <xintongyang1994@foxmail.com>
2024-09-17 10:41:35 +0100

    results

--- scripts/run_cuda_1.sh
+++ scripts/run_cuda_1.sh
@@ -1,14 +1,14 @@
-python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 0
-python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 1
-python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 2
-python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 3
-python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 4
+# python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 0
+# python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 1
+# python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 2
+# python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 3
+# python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 4
 
-python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 0 --demon
-python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 1 --demon
-python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 2 --demon
-python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 3 --demon
-python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 4 --demon
+# python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 0 --demon
+# python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 1 --demon
+# python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 2 --demon
+# python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 3 --demon
+# python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 4 --demon
 
 python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 0 --demon --mini-batch
 python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 1 --seed 1 --demon --mini-batch
