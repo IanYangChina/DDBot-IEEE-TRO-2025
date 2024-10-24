f63b93ce85e874d555d68ddfdb7923becab8af9a
Xintong <xintongyang1994@foxmail.com>
2024-10-24 09:47:55 +0100

    script

--- scripts/run_cuda_0.sh
+++ scripts/run_cuda_0.sh
@@ -12,6 +12,20 @@ python assess_grad.py --ptcl_d 5e6 --cuda_GB 5 --res 40 --substep
 python assess_grad.py --ptcl_d 5e6 --cuda_GB 5 --res 50 --substep
 python assess_grad.py --ptcl_d 5e6 --cuda_GB 5 --res 60 --substep
 
+python assess_grad.py --ptcl_d 5e6 --cuda_GB 5 --res 10 --grad-clip
+python assess_grad.py --ptcl_d 5e6 --cuda_GB 5 --res 20 --grad-clip
+python assess_grad.py --ptcl_d 5e6 --cuda_GB 5 --res 30 --grad-clip
+python assess_grad.py --ptcl_d 5e6 --cuda_GB 5 --res 40 --grad-clip
+python assess_grad.py --ptcl_d 5e6 --cuda_GB 5 --res 50 --grad-clip
+python assess_grad.py --ptcl_d 5e6 --cuda_GB 5 --res 60 --grad-clip
+
+python assess_grad.py --ptcl_d 5e6 --cuda_GB 5 --res 10 --grad-clip --substep
+python assess_grad.py --ptcl_d 5e6 --cuda_GB 5 --res 20 --grad-clip --substep
+python assess_grad.py --ptcl_d 5e6 --cuda_GB 5 --res 30 --grad-clip --substep
+python assess_grad.py --ptcl_d 5e6 --cuda_GB 5 --res 40 --grad-clip --substep
+python assess_grad.py --ptcl_d 5e6 --cuda_GB 5 --res 50 --grad-clip --substep
+python assess_grad.py --ptcl_d 5e6 --cuda_GB 5 --res 60 --grad-clip --substep
+
 #python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 0 --seed 0 --lr 0.02
 #python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 0 --seed 1 --lr 0.02
 #python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 8 --task-id 0 --seed 2 --lr 0.02
