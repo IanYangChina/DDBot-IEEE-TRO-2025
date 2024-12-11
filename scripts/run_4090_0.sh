392445712d56a29b10943a7685dd7888324d070c
Xintong <xintongyang1994@foxmail.com>
2024-12-11 09:49:56 +0000

    data processing; new scripts

--- scripts/run_4090_0.sh
+++ scripts/run_4090_0.sh
@@ -9,3 +9,4 @@ python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 12 --task-id 0 --line-search --lr 0
 python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 12 --task-id 1 --line-search --lr 0.03 --demon --hm --sand
 python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 12 --task-id 2 --line-search --lr 0.03 --demon --hm --sand
 
+python run_so_abs2.py --ptcl_d 5e6 --cuda_GB 12 --task-id 0 --line-search --lr 0.03 --sand --init-search
