ea1402ef69d5c2c38e6b4004ea869bfbf9982af6
Xintong <xintongyang1994@foxmail.com>
2024-12-13 17:18:02 +0000

    sac render

---
+++ scripts/testing/run_4090.sh
@@ -0,0 +1,19 @@
+export CUDA_VISIBLE_DEVICES=1
+python test_rendering.py --sac --task-id 0 --simg --shm --seed 0
+python test_rendering.py --sac --task-id 0 --simg --shm --seed 1
+python test_rendering.py --sac --task-id 0 --simg --shm --seed 2
+python test_rendering.py --sac --task-id 0 --simg --shm --seed 3
+
+python test_rendering.py --sac --task-id 1 --simg --shm --seed 0
+python test_rendering.py --sac --task-id 1 --simg --shm --seed 1
+python test_rendering.py --sac --task-id 1 --simg --shm --seed 2
+python test_rendering.py --sac --task-id 1 --simg --shm --seed 3
+
+python test_rendering.py --sac --task-id 2 --simg --shm --seed 0
+python test_rendering.py --sac --task-id 2 --simg --shm --seed 1
+python test_rendering.py --sac --task-id 2 --simg --shm --seed 2
+python test_rendering.py --sac --task-id 2 --simg --shm --seed 3
+
+rm -rf ../../log-abs2-sac/d5e6-task-0-her-demo/data
+rm -rf ../../log-abs2-sac/d5e6-task-1-her-demo/data
+rm -rf ../../log-abs2-sac/d5e6-task-2-her-demo/data
