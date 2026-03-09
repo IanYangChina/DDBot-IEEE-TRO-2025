a02975a3a7ceefc8181f802c5a2fa10c09ce5e58
Xintong <xintongyang1994@foxmail.com>
2026-03-09 11:51:58 +0000

    rg experiments

--- scripts/run_2080_rg.sh
+++ scripts/run_2080_rg.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 #
 #python run_so_abs2.py --task-id 0 --line-search --lr 0.03 --rgrad --seed 0
 #python run_so_abs2.py --task-id 0 --line-search --lr 0.03 --rgrad --seed 1
@@ -125,9 +125,9 @@ export CUDA_VISIBLE_DEVICES=0
 
 # task 2
 #python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 0 --sand
-python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 1 --sand
-python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 2 --sand
-python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 3 --sand
+#python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 1 --sand
+#python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 2 --sand
+#python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 3 --sand
 python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 4 --sand
 
 python run_so_abs2.py --task-id 2 --line-search --demon --lr 0.03 --rgrad --seed 0 --sand
@@ -136,14 +136,14 @@ python run_so_abs2.py --task-id 2 --line-search --demon --lr 0.03 --rgrad --seed
 python run_so_abs2.py --task-id 2 --line-search --demon --lr 0.03 --rgrad --seed 3 --sand
 python run_so_abs2.py --task-id 2 --line-search --demon --lr 0.03 --rgrad --seed 4 --sand
 
-python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 0 --hm --sand
-python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 1 --hm --sand
-python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 2 --hm --sand
-python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 3 --hm --sand
-python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 4 --hm --sand
+#python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 0 --hm --sand
+#python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 1 --hm --sand
+#python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 2 --hm --sand
+#python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 3 --hm --sand
+#python run_so_abs2.py --task-id 2 --line-search --lr 0.03 --rgrad --seed 4 --hm --sand
 
-python run_so_abs2.py --task-id 2 --line-search --demon --lr 0.03 --rgrad --seed 0 --hm --sand
-python run_so_abs2.py --task-id 2 --line-search --demon --lr 0.03 --rgrad --seed 1 --hm --sand
-python run_so_abs2.py --task-id 2 --line-search --demon --lr 0.03 --rgrad --seed 2 --hm --sand
-python run_so_abs2.py --task-id 2 --line-search --demon --lr 0.03 --rgrad --seed 3 --hm --sand
-python run_so_abs2.py --task-id 2 --line-search --demon --lr 0.03 --rgrad --seed 4 --hm --sand
+#python run_so_abs2.py --task-id 2 --line-search --demon --lr 0.03 --rgrad --seed 0 --hm --sand
+#python run_so_abs2.py --task-id 2 --line-search --demon --lr 0.03 --rgrad --seed 1 --hm --sand
+#python run_so_abs2.py --task-id 2 --line-search --demon --lr 0.03 --rgrad --seed 2 --hm --sand
+#python run_so_abs2.py --task-id 2 --line-search --demon --lr 0.03 --rgrad --seed 3 --hm --sand
+#python run_so_abs2.py --task-id 2 --line-search --demon --lr 0.03 --rgrad --seed 4 --hm --sand
